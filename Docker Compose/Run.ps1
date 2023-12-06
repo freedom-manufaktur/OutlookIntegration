ea6f19b0d4b4a0660dc6acbbd87b346d74dccc0f
Martin <martin@freedom-manufaktur.com>
2023-12-06 18:41:28 +0100

    Add compose and HELM

---
+++ Docker Compose/Run.ps1
@@ -0,0 +1,2 @@
+# Run in background (detachted from terminal)
+docker compose --env-file ./compose.env --env-file ./compose.env.user up --detach
---
+++ Docker Compose/Stop.ps1
@@ -0,0 +1,1 @@
+docker compose --env-file ./compose.env --env-file ./compose.env.user down
