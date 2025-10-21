898657a0068710f37f5416e4f1b365ac3fd568f4
Martin Freiwald <martin@freedom-manufaktur.com>
2025-10-21 17:52:27 +0200

    Update docker ps1s

--- Docker Compose/Run.ps1
+++ Docker Compose/Run.ps1
@@ -1,2 +1,9 @@
-# Run in background (detachted from terminal)
-docker compose --env-file ./compose.env --env-file ./compose.env.user up --detach
+﻿# Run in background (detachted from terminal)
+if ((Test-Path -Path "compose.env.user"))
+{
+  docker compose --env-file ./compose.env --env-file ./compose.env.user up --detach
+}
+else
+{
+  docker compose --env-file ./compose.env up --detach
+}
